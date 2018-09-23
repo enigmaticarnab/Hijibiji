fb25d3226f1c82fae8b9301f94e31beeb09e2211
ARNAB <ARNAB@DESKTOP-RGJVMQJ>
2018-09-23 13:03:03 +0530

    This is 2nd commit: Changing the pritln message type

--- NewProjecct/src/arnab/Test.java
+++ NewProjecct/src/arnab/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is arnab first commit");
+		System.out.println("This is arnab 2nd commit");
 		
 	}
 
