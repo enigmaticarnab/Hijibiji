975d55d3f10e05c430ff621e5e02db41c764c59e
ARNAB <ARNAB@DESKTOP-RGJVMQJ>
2019-02-27 08:46:13 +0530

    for new check in

--- test123/src/test/test.java
+++ test123/src/test/test.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class test {
-
+//For git hub demo
 	public static void main(String[] args) throws ClassNotFoundException {
 		// TODO Auto-generated method stub
 		//String exePath = "C:\\Users\\ARNAB\\Downloads\\chromedriver.exe";
