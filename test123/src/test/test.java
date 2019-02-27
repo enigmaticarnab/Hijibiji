9b5ddb913b3ada9fd30f8ae063070328ed304a15
ARNAB <ARNAB@DESKTOP-RGJVMQJ>
2019-02-27 08:17:47 +0530

    first commit for the second project

---
+++ test123/src/test/test.java
@@ -0,0 +1,34 @@
+package test;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		// TODO Auto-generated method stub
+		//String exePath = "C:\\Users\\ARNAB\\Downloads\\chromedriver.exe";
+		//System.setProperty("webdriver.chrome.driver", exePath);
+		//ChromeDriver driver = new ChromeDriver();
+		//driver.get("https://www.google.co.in");
+		
+		String url = "jdbc:sqlserver://DESKTOP-RGJVMQJ\\SQLEXPRESS;databaseName=arnab;integratedSecurity=true";
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		try {
+			
+			Connection conn = DriverManager.getConnection(url);
+			System.out.println("The connection string is : " +conn);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
